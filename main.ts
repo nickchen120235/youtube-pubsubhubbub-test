1799fd62c2d69692f65027ab018cbcb4eaa01880
Nickchen Nick <nickchen120235@gmail.com>
2024-05-16 01:29:32 +0800

    add kv and update sub logic

--- main.ts
+++ main.ts
@@ -5,6 +5,11 @@ await dotenv.load({ export: true })
 const webhook = Deno.env.get('WEBHOOK')
 if (!webhook)
     throw new Error('WEBHOOK environment variable not set')
+const callback = Deno.env.get('CALLBACK')
+if (!callback)
+    throw new Error('CALLBACK environment variable not set')
+
+const kv = await Deno.openKv(Deno.env.get('KV_PATH'))
 
 Deno.serve(async (req) => {
     switch (req.method) {
@@ -12,13 +17,18 @@ Deno.serve(async (req) => {
             console.log('Received callback challenge')
             console.log(`URL: ${req.url}`)
             const url = new URL(req.url)
+            const topic = url.searchParams.get('hub.topic')
             const challenge = url.searchParams.get('hub.challenge')
-            if (challenge) return new Response(challenge, {
-                status: 200,
-                headers: {
-                    'Content-Type': 'text/plain'
-                },
-            })
+            const lease = url.searchParams.get('hub.lease_seconds')
+            if (topic && challenge && lease) {
+                await kv.set(['topic', decodeURI(topic)], parseInt(lease) + Math.floor(Date.now() / 1000))
+                return new Response(challenge, {
+                    status: 200,
+                    headers: {
+                        'Content-Type': 'text/plain'
+                    },
+                })
+            }
             else return new Response(null, { status: 400 })
         }
         case 'POST': {
@@ -57,6 +67,40 @@ Deno.serve(async (req) => {
             console.log('Received unknown request')
             console.log(`${req.method} ${req.url}`)
             return new Response(null, { status: 405 })
+        }
+    }
+})
+
+Deno.cron('Update subscriptions', '0 0 * * *', async () => {
+    for await (const entry of kv.list<number>({ prefix: ['topic'] })) {
+        try {
+            const { key, value } = entry
+            const [_, topic] = key as [string, string]
+            if (value - Math.floor(Date.now() / 1000) < 86400) {
+                console.log(`Updating subscriptions for ${topic}`)
+                const form = new FormData()
+                form.set('hub.callback', callback)
+                form.set('hub.topic', topic)
+                form.set('hub.verify', 'async')
+                form.set('hub.mode','subscribe')
+                form.set('hub.verify_token', '')
+                form.set('hub.secret', '')
+                form.set('hub.lease_numbers', '')
+                const res = await fetch('https://pubsubhubbub.appspot.com/subscribe', {
+                    method: 'POST',
+                    body: form
+                })
+                if (!res.ok)
+                    throw new Error(`${res.status} ${res.statusText}`)
+            }
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                console.error(`Error: ${e.message}`)
+            }
+            else {
+                console.error(`Error: ${JSON.stringify(e)}`)
+            }
         }
     }
 })
